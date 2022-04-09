67c6fbd8e2bd084252a2fedf97e97144008824cd
Artem Paliutin <pal.artem@icloud.com>
2022-04-09 16:28:48 +0300

    Initial Commit

---
+++ VideoPlayerJson/VideoPlayerJsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerJsonApp.swift
+//  VideoPlayerJson
+//
+//  Created by Artem Paliutin on 09/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerJsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
